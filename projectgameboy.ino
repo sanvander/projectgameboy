ba54e05af5a349239e52587663f83f1bccd0daab
sander van der werff <sander-werff@hotmail.com>
2024-01-09 13:05:58 +0100

    Add logic for two buttons

--- projectgameboy.ino
+++ projectgameboy.ino
@@ -1,90 +1,95 @@
-/***************************************************
-  This is our GFX example for the Adafruit ILI9341 Breakout and Shield
-  ----> http://www.adafruit.com/products/1651
-
-  Check out the links above for our tutorials and wiring diagrams
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional)
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-
-
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
 
-// For the Adafruit shield, these are the default.
+// Define pins for the buttons
+#define RIGHT_BUTTON_PIN 2
+#define LEFT_BUTTON_PIN 1
+
+// Display pins
 #define TFT_DC 9
 #define TFT_CS 10
 #define TFT_RST 8
 
-const int buttonPin = 2;
-int buttonState;         
-int lastButtonState = LOW;
+// Button state variables
+int rightButtonState;         
+int lastRightButtonState = LOW;
+int leftButtonState;         
+int lastLeftButtonState = LOW;
+
+// Debounce variables
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
-const int interval = 50; // Interval in milliseconden (1 seconde)
-unsigned long previousMillis = 0;
 
+// Other variables
+const int interval = 50; // Interval in milliseconds
+unsigned long previousMillis = 0;
 int button_clicks = 0;
 
-// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
+// Initialize display
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
-// If using the breakout, change pins as desired
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
 void setup() {
-   pinMode(2, INPUT);
+  pinMode(RIGHT_BUTTON_PIN, INPUT);
+  pinMode(LEFT_BUTTON_PIN, INPUT);
   
   tft.begin();
-  tft.setRotation(2); // Adjust to your screen orientation
+  tft.setRotation(0);
   tft.fillScreen(ILI9341_BLACK);
   tft.setTextColor(ILI9341_WHITE);
   tft.setTextSize(2);
   tft.println(button_clicks);
-  
 }
 
 void loop() {
-  int reading = digitalRead(buttonPin);
+  int rightReading = digitalRead(RIGHT_BUTTON_PIN);
+  int leftReading = digitalRead(LEFT_BUTTON_PIN);
   unsigned long currentMillis = millis();
 
-  if (reading != lastButtonState) {
+  // Debounce logic for the right button
+  if (rightReading != lastRightButtonState) {
     lastDebounceTime = millis();
   }
-
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      if (buttonState == HIGH) {
-        button_clicks = button_clicks + 1;
-        tft.setCursor(0, 0);
-        clearLine(0);
-        tft.println(button_clicks);
-        
+    if (rightReading != rightButtonState) {
+      rightButtonState = rightReading;
+      if (rightButtonState == HIGH) {
+        // Right button logic
+        button_clicks++;
+        updateDisplay();
       }
     }
   }
-  lastButtonState = reading;
+  lastRightButtonState = rightReading;
 
+  // Debounce logic for the left button
+  if (leftReading != lastLeftButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (leftReading != leftButtonState) {
+      leftButtonState = leftReading;
+      if (leftButtonState == HIGH) {
+        // Left button logic
+        button_clicks--;
+        updateDisplay();
+      }
+    }
+  }
+  lastLeftButtonState = leftReading;
 
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
   }
-  
-
-
-
 }
 
-void clearLine(int y) {
-  tft.fillRect(0, y, tft.width(), 20, ILI9341_BLACK);
-  // tft.textHeight() geeft de hoogte van de huidige tekstgrootte
+void updateDisplay() {
+  tft.setCursor(0, 0);
+  clearLine(0);
+  tft.println(button_clicks);
 }
 
+void clearLine(int line) {
+  // Clear the line on the display where the text is printed
+  tft.fillRect(0, line * 16, tft.width(), 16, ILI9341_BLACK);
+}
